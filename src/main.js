d357dd09d3519444dce20d83ccb2ba709ed824c8
Camille <128691241+chua2201@users.noreply.github.com>
2023-12-15 01:57:01 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,14 +15,14 @@ import VueGoogleMaps from '@fawmi/vue-google-maps';
 
 // import firebase from "firebase/compat/app"; 
 const firebaseConfig = {
-    apiKey: "AIzaSyDmN08IvkUv2vQn7o_Cbd5daeQ3OXxGRpo",
-    authDomain: "ecoscape-ea1d6.firebaseapp.com",
-    databaseURL: "https://ecoscape-ea1d6-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "ecoscape-ea1d6",
-    storageBucket: "ecoscape-ea1d6.appspot.com",
-    messagingSenderId: "104462396856",
-    appId: "1:104462396856:web:bcaaa850fb71afed13b34d",
-    measurementId: "G-35CF9R9L0S"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   const app = createApp(App);
@@ -40,7 +40,7 @@ app.use(router);
 app.use(store);
 app.use(VueGoogleMaps, {
     load: {
-        key: 'AIzaSyDmN08IvkUv2vQn7o_Cbd5daeQ3OXxGRpo',
+        key: '',
         libraries: "places", // Include additional libraries as needed
 
     },
